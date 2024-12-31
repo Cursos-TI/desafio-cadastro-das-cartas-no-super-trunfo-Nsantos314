e9ce049c056d8a0d7f3aa700eccd36f457952d85
Nsantos314 <nadsonsantosdasilva503@gmail.com>
2024-12-31 04:40:47 +0000

    scanf codigo carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,35 @@
 //Teste larissa
 
 int main() {
+    char estado = 'A';
+    char codigo[20] = "A01";
+    int pessoas = 123456;
+    float area = 10.000;
+    float pib = 21.600;
+    int turismo = 5;
+
+    printf("estado %c\n", estado);
+    printf("codigo %e\n", codigo);
+    printf("população %d\n", pessoas);
+    printf("area %f\n", area);
+    printf("pib %f\n", pib);
+    printf("turismo %d\n", pib);
+
+    print("Digite o nome do estado\n");
+    scanf("Estado %c"\n, &estado);
+    printf("Digite o codigo da carta\n");
+    scanf("")
+printf("Digite o numero da população\n");
+
+
+
+    return 0;
+}
+    
+
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -18,5 +47,5 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
-}
+    
+
